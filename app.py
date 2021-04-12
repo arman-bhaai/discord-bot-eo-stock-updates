02584a6138865ca94679b7fcdf730efb90aa7a6c
Abu Bakar Siddique Arman <arman.bhaai@gmail.com>
2021-04-12 14:06:01 +0600

    modified stock increase feature

--- app.py
+++ app.py
@@ -153,19 +153,20 @@ class MyCog(Cog):
                             elif i['stock'] <= 0 and j['stock'] > 0:
                                 # product stocked out
                                 self.print_log('product stocked out')
-                                for idx3, k in enumerate(self.lst_stocked_in):
-                                    if k['name'] == i['name']:
-                                        self.lst_stocked_in.pop(idx3)
+                                #for idx3, k in enumerate(self.lst_stocked_in):
+                                #    if k['name'] == i['name']:
+                                #        self.lst_stocked_in.pop(idx3)
                                 # products_stocked_out.append(j)
                                 # fields = [("Stocks", i['stock'], True), ('Price', i['price'], True),]
                                 # self.bot_send_embed(channel, fields, evt_loop, i['name'], author_name='Stocked Out!', colour=0xFF0000)
-                            elif i['stock'] > j['stock']:
-                                for idx4, l in enumerate(self.lst_stocked_in):
-                                    if i['name'] == l['name']:
-                                        self.print_log('product stock increased')
-                                        fields = [("Stocks", i['stock'], True), ('Price', i['price'], True),]
-                                        self.bot_send_embed(channel, fields, evt_loop, i['name'], author_name='Stock Increased!', colour=0x1E90FF)
-                                        self.lst_stocked_in[idx4] = i
+                            elif i['stock'] > j['stock'] and j['stock'] >  0:
+                                #for idx4, l in enumerate(self.lst_stocked_in):
+								#	if i['name'] == l['name']:
+								self.print_log('product stock increased')
+								fields = [("Stocks", i['stock'], True), ('Price', i['price'], True),]
+								self.bot_send_embed(channel, fields, evt_loop, i['name'], author_name='Stock Increased!', colour=0x1E90FF)
+								#self.lst_stocked_in[idx4] = i
+                          
 
         return db_list_products_new
         
